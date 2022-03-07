acf90b5f03cd298f5f2c5ca62fd428ce3a5f8cca
izhaong <izhaong@outlook.com>
2022-03-07 19:59:35 +0800

    feat: add normalize.css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 仲灏<izhaong@outlook.com>🌶🌶🌶
  * @Date: 2022-03-07 13:47:05
- * @LastEditTime: 2022-03-07 14:10:31
+ * @LastEditTime: 2022-03-07 19:55:55
  * @LastEditors: 仲灏<izhaong@outlook.com>🌶🌶🌶
  * @Description:
  * @FilePath: /vue-template/src/main.ts
@@ -13,6 +13,7 @@ import { createPinia } from 'pinia'
 import { Button, Loading, Empty } from 'vant'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
+import 'normalize.css'
 
 const app = createApp(App)
 app.use(createPinia()).use(router)

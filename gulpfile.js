6d10ade1b2b950eefe333079bf128669fe72aad8
仲灏 <izhaong@outlook.com>
2024-08-19 23:00:19 +0800

    feat: add theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,44 @@
-var gulp   = require('gulp')
-var $      = require('gulp-load-plugins')()
-var fs     = require('fs')
-var path   = require('path')
-var del    = require('del')
-//var colors = require('colors')
+var gulp = require('gulp')
+var $ = require('gulp-load-plugins')()
+var fs = require('fs')
+var path = require('path')
+var del = require('del')
+// var colors = require('colors')
 var child_process = require('child_process')
 
-var theme            = {}
-var themeList        = require('./src/element-ui/config.js').filter(item => !item.hasBuild)
-var styleFileDir     = './src/assets/scss'
+var theme = {}
+var themeList = require('./src/element-ui/config.js').filter((item) => !item.hasBuild)
+var styleFileDir = './src/assets/scss'
 var styleFileDirTemp = `${styleFileDir}-temp`
-var themeFileDir     = './public/element-theme'
-var et               = require('element-theme')
-var etOptions        = require('./package.json')['element-theme']
-var themeFileName    = etOptions.config.replace(/.*\/(.+\.scss)/, '$1')
+var themeFileDir = './public/element-theme'
+var et = require('element-theme')
+var etOptions = require('./package.json')['element-theme']
+var themeFileName = etOptions.config.replace(/.*\/(.+\.scss)/, '$1')
 
 /**
  * 构建生成主题
  */
 gulp.task('themes', () => {
-  if (themeList.length <= 0) { return del(styleFileDirTemp) }
+  if (themeList.length <= 0) {
+    return del(styleFileDirTemp)
+  }
 
   // 删除临时文件，保证本次操作正常执行
-  //del(styleFileDirTemp)
-  console.log('del(styleFileDirTemp)');
+  // del(styleFileDirTemp)
+  console.log('del(styleFileDirTemp)')
 
   // 拷贝一份scss样式文件夹，作为构建的临时处理文件夹
-  //child_process.spawnSync('cp', ['-r', styleFileDir, styleFileDirTemp])
-  console.log('copy styleFileDirTemp');
+  // child_process.spawnSync('cp', ['-r', styleFileDir, styleFileDirTemp])
+  console.log('copy styleFileDirTemp')
 
   // 拷贝element组件scss变量样式文件至临时处理文件夹中，并修改相应配置信息
-  //child_process.spawnSync('cp', ['-r', etOptions.config, styleFileDirTemp])
+  // child_process.spawnSync('cp', ['-r', etOptions.config, styleFileDirTemp])
   etOptions.config = `${styleFileDirTemp}/${themeFileName}`
 
   // 开始构建生成
   fnCreate(themeList)
 
-  function fnCreate (themeList) {
+  function fnCreate(themeList) {
     if (themeList.length >= 1) {
       // 保存当前构建生成的主题对象
       theme = themeList[0]
@@ -78,12 +80,7 @@ gulp.task('themes', () => {
       console.log('\n')
 
       // 删除主题不需要的部分文件
-      var files = [
-        `${themeFileDir}/**/*.css`,
-        `!${themeFileDir}/**/index.css`,
-        `!${themeFileDir}/**/aui.css`,
-        `!${themeFileDir}/**/fonts`
-      ]
+      var files = [`${themeFileDir}/**/*.css`, `!${themeFileDir}/**/index.css`, `!${themeFileDir}/**/aui.css`, `!${themeFileDir}/**/fonts`]
       del(files)
       console.log('-------------------- 构建完毕，删除主题独立组件文件 -------------------------')
       console.log(files)
@@ -93,12 +90,15 @@ gulp.task('themes', () => {
 })
 
 gulp.task('styles', () => {
-  return gulp.src([`${styleFileDirTemp}/aui.scss`])
+  return gulp
+    .src([`${styleFileDirTemp}/aui.scss`])
     .pipe($.sass().on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: etOptions.browsers,
-      cascade: false
-    }))
+    .pipe(
+      $.autoprefixer({
+        browsers: etOptions.browsers,
+        cascade: false
+      })
+    )
     .pipe($.cleanCss())
     .pipe($.rename('aui.css'))
     .pipe(gulp.dest(`${themeFileDir}/${theme.name}`))
